0351ab28f9385810dd04fb73651227f66125f394
Rizki <emrizky804@gmail.com>
2025-11-11 16:27:22 +0700

    feat: add project page

---
+++ Frontend/app/common/card.tsx
@@ -0,0 +1,75 @@
+interface CardProps {
+  image: string;
+  date: string;
+  info: string;
+  title: string;
+  desc: string;
+  tags: string[];
+}
+
+export default function Card({
+  image,
+  date,
+  info,
+  title,
+  desc,
+  tags,
+}: CardProps) {
+  return (
+    <div
+      className="group rounded-2xl overflow-hidden 
+                 border border-gray-200 shadow-sm"
+    >
+      <div className="relative overflow-hidden">
+        {/* gambar */}
+        <img
+          src={image}
+          alt={title}
+          className="w-full aspect-video object-cover transition-transform 
+                     duration-500 group-hover:scale-110"
+        />
+
+        {/* overlay gelap */}
+        <div
+          className="absolute inset-0 bg-black/50 opacity-0 
+                     group-hover:opacity-100 transition-opacity duration-500"
+        />
+
+        {/* hover atas - date and location */}
+        <div
+          className="absolute top-4 left-4 z-10 opacity-0 -translate-y-4
+                     group-hover:opacity-100 group-hover:translate-y-0
+                     transition-all duration-500
+                     flex items-center gap-2" // <-- Dibungkus flex
+        >
+          {/* date */}
+          <span className="bg-orange-500 text-white text-xs px-2 py-1 rounded">
+            {date}
+          </span>
+
+          {/* location */}
+          <span className="bg-orange-500 text-white text-xs px-2 py-1 rounded">
+            {info}
+          </span>
+        </div>
+      </div>
+
+      {/* bagian bawah */}
+      <div className="bg-white p-4">
+        <h3 className="text-xl font-semibold text-gray-900">{title}</h3>
+        <p className="text-sm text-gray-600 mt-1 line-clamp-2">{desc}</p>
+        <div className="flex flex-wrap gap-2 mt-3">
+          {tags.map((t, i) => (
+            <span
+              key={i}
+              className="bg-orange-500 text-white text-xs 
+                         px-2 py-1 rounded"
+            >
+              {t}
+            </span>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
--- Frontend/app/components/Proyek/coba.tsx
+++ Frontend/app/components/Proyek/coba.tsx
@@ -3,33 +3,33 @@ import { Tag, Tags } from "lucide-react";
 import { Search } from "lucide-react";
 import { Funnel } from "lucide-react";
 import { useState } from "react";
+import Card from "../../common/card";
 
 export function Coba() {
   const projects = [
     {
-      id: 1,
+      image: "/home/test2.jpg",
+      date: "11 Nov 2025",
       title: "Project A",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-      image: "/home/test2.jpg",
-      link: "#",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      tags: ["Tailwind"],
+      info: "Game",
     },
     {
-      id: 2,
+      image: "/home/test2.jpg",
+      date: "11 Nov 2025",
       title: "Project B",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-      image:
-        "https://images.pexels.com/photos/669615/pexels-photo-669615.jpeg?auto=compress&cs=tinysrgb&w=500",
-      link: "#",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      tags: ["Tailwind", "React"],
+      info: "UI / UX",
     },
     {
-      id: 3,
-      title: "Project C",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
       image: "/home/test2.jpg",
-      link: "#",
+      date: "11 Nov 2025",
+      title: "Project C",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      tags: ["Tailwind", "Javascript"],
+      info: "AR / VR",
     },
   ];
 
@@ -41,7 +41,7 @@ export function Coba() {
         <main className="flex items-center justify-center py-10">
           <section id="intro">
             <div className="max-w-6xl mx-auto px-6">
-              <div className="flex gap-4">
+              <div className="flex gap-4 items-center ">
                 <div className="flex items-center bg-[#f5ece5] rounded-lg px-3 py-2 w-full max-w-md shadow-sm mb-10">
                   <Search className="stroke-black" />
                   <input
@@ -56,12 +56,12 @@ export function Coba() {
                     className="bg-[#f5ece5] flex-1 ml-2 focus:outline-none text-black placeholder-black cursor-pointer"
                     defaultValue=""
                   >
-                    {" "}
                     <option value="" disabled>
                       Select Category
                     </option>
                     <option value="ui/ux">UI/UX</option>
                     <option value="game">Game</option>
+                    <option value="ar/vr">AR/VR</option>
                   </select>
                 </div>
 
@@ -70,12 +70,13 @@ export function Coba() {
                     className="bg-[#f5ece5] flex-1 ml-2 focus:outline-none text-black placeholder-black cursor-pointer"
                     defaultValue=""
                   >
-                    {" "}
                     <option value="" disabled>
-                      Select
+                      Tags
                     </option>
                     <option value="react">React</option>
                     <option value="tailwind">Tailwind</option>
+                    <option value="unity">Unity</option>
+                    <option value="Figma">Figma</option>
                   </select>
                 </div>
 
@@ -84,7 +85,6 @@ export function Coba() {
                     className="bg-[#f5ece5] flex-1 ml-2 focus:outline-none text-black placeholder-black cursor-pointer"
                     defaultValue=""
                   >
-                    {" "}
                     <option value="" disabled>
                       Year
                     </option>
@@ -95,47 +95,34 @@ export function Coba() {
                 </div>
 
                 <div
-                  className={`flex items-center bg-[#f5ece5] rounded-lg px-2 py-1 max-w-sm shadow-sm mb-10 ${selectedOption ? "w-fit min-w-[90px]" : "w-10 justify-center"}`}
+                  className={`flex items-center bg-[#f5ece5] rounded-lg px-3 py-2 max-w-sm shadow-sm mb-10 ${selectedOption ? "w-fit min-w-30" : "w-30 justify-center"}`}
                 >
                   {!selectedOption && (
-                    <Funnel size={1} className="stroke-black" />
+                    <Funnel size={20} className="stroke-black" />
                   )}
                   <select
                     className="bg-[#f5ece5] flex-1 ml-1 text-sm focus:outline-none text-black placeholder-black cursor-pointer"
                     defaultValue=""
                     onChange={(e) => setSelectedOption(e.target.value)}
                   >
-                    {" "}
-                    <option value="" disabled></option>
+                    <option value="" disabled>
+                      Sort by
+                    </option>
+                    <option value="popular">Popular</option>
                     <option value="old">Oldest</option>
                     <option value="latest">Latest</option>
+                    <option value="a-z">A - Z</option>
+                    <option value="z-a">Z - A</option>
                   </select>
                 </div>
               </div>
 
-              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-                {projects.map((project) => (
-                  <div
-                    key={project.id}
-                    className="bg-white rounded-lg border border-orange-200 hover:border-orange-300 hover:shadow-lg hover:shadow-sky-200/30 transition-all overflow-hidden"
-                  >
-                    <div className="aspect-video overflow-hidden">
-                      <img
-                        src={project.image}
-                        alt={project.title}
-                        className="w-full h-full object-cover hover:scale-105 transition-transform duration-300"
-                      />
-                    </div>
-                    <div className="p-6">
-                      <h3 className="text-lg font-semibold text-gray-900 mb-2">
-                        {project.title}
-                      </h3>
-                      <p className="text-gray-600 text-sm mb-4 line-clamp-2">
-                        {project.description}
-                      </p>
-                    </div>
-                  </div>
-                ))}
+              <div className="min-h-screen bg-white px-6 py-4">
+                <div className="grid md:grid-cols-3 gap-8">
+                  {projects.map((e, i) => (
+                    <Card key={i} {...e} />
+                  ))}
+                </div>
               </div>
             </div>
           </section>

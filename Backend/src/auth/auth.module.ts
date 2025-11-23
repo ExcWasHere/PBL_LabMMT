f48970c1f8c06cf13040c7b9186111cfb3cdf96b
Your Name <excellchriatian12@gmail.com>
2025-11-23 23:28:27 +0700

    fix(auth): jwt login

--- Backend/src/auth/auth.module.ts
+++ Backend/src/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from '../user/user.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
@@ -22,7 +23,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       },
     }),
   ],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   controllers: [AuthController],
   exports: [AuthService],
 })

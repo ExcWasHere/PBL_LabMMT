8a3f5117a2aee146d50cac7cc7faefef986fa670
Your Name <excellchriatian12@gmail.com>
2025-12-01 17:03:36 +0700

    chore: clean the code

--- Backend/src/galeri/gallery-image.entity.ts
+++ Backend/src/galeri/gallery-image.entity.ts
@@ -1,17 +1,16 @@
-import {Entity, PrimaryGeneratedColumn, Column, ManyToOne} from "typeorm";
-import { Gallery } from "./gallery.entity";
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
+import { Gallery } from './gallery.entity';
 
 @Entity()
 export class GalleryImage {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ type: "varchar", length: 255 })
+  @Column({ type: 'varchar', length: 255 })
   fileUrl: string;
 
   @ManyToOne(() => Gallery, (gallery) => gallery.images, {
-    onDelete: "CASCADE",
+    onDelete: 'CASCADE',
   })
   gallery: Gallery;
 }
-

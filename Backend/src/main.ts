54efb603ab433b7f81ab76bce32f1ae8c59d43f2
Your Name <excellchriatian12@gmail.com>
2025-12-02 09:14:55 +0700

    feat: add drag n drop cv

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -1,5 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
+import * as path from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +11,12 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
+
+  const uploadDir = path.join(process.cwd(), 'uploads', 'cv');
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+    console.log('✅ Folder uploads/cv berhasil dibuat!');
+  }
 
   await app.listen(3000);
   console.log('🚀 Server running on http://localhost:3000');
